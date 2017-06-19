5484bbf80814574cd517ff4b61f1044b22fab3eb
derushio <narumi18wa@gmail.com>
2017-06-19 18:12:07 +0900

    fix script

--- install-npm-script.js
+++ install-npm-script.js
@@ -2,9 +2,10 @@ const fs = require('fs')
 
 var json = JSON.parse(fs.readFileSync('./package.json', 'utf8'))
 
-json["build"] = "node tool/build/build.js"
-json["webpack"] = "webpack --progress"
-json["build-pug"] = "pug --hierarchy -o ./dist/ ./src/"
-json["build-sass"] = "./tool/build/build-sass.sh"
+json["scripts"] = {}
+json["scripts"]["build"] = "node tool/build/build.js"
+json["scripts"]["webpack"] = "webpack --progress"
+json["scripts"]["build-pug"] = "pug --hierarchy -o ./dist/ ./src/"
+json["scripts"]["build-sass"] = "./tool/build/build-sass.sh"
 
 fs.writeFileSync("./package.json", JSON.stringify(json, null, 4));
