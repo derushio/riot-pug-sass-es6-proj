64e60adeba109d18a904e4a09b5769b0a85cd78d
derushio <narumi18wa@gmail.com>
2017-06-19 15:10:02 +0900

    webpack riot

--- src/script/entry/index.js
+++ src/script/entry/index.js
@@ -1,1 +1,3 @@
+import Test from '../tag/test.tag';
+
 console.log("test");
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import webpack from 'webpack';
 
 export default {
     context: path.join(__dirname, "./src/script/entry"),
@@ -8,5 +9,42 @@ export default {
     output: {
         path: path.join(__dirname, "./dist/script/entry"),
         filename: "[name].bundle.js"
-    }
-};
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tag$/,
+                enforce: 'pre',
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'riot-tag-loader',
+                        options: {
+                            template: 'pug',
+                            debug: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.js|\.tag$/,
+                enforce: 'post',
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: `es2015-riot`
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.tag']
+    },
+    plugins: [
+        new webpack.ProvidePlugin({ riot: 'riot' })
+    ]
+}
